5d97c167d0475e8f9704f9a5fb48e665c6bf3228
Antonios Kaloskamis <antoniskaloskamis@gmail.com>
2019-09-18 17:43:19 +0200

    LeapYears

---
+++ LeapYear.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 17:25:32 2019
+
+@author: user
+"""
+
+def leap_year(year):
+    if year % 400==0 or (year % 4 == 0 and year % 100 !=0):
+        return '{} is a leap year'.format(year)
+    else:
+        return '{} is not a leap year'.format(year)
+    
+print (leap_year(2019))
